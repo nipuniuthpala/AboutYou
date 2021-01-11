ded16676a0d720ae75c911b4d5ce9ae68d8fa8eb
nipuni.gunatilake <nipuni.gunatilake@rakuten.com>
2021-01-11 16:25:00 +0900

    modified file

--- support/ReadMe.js
+++ support/ReadMe.js
@@ -13,32 +13,13 @@
 
 {
 "chromeWebSecurity": false,
-"projectId": "cpve1v"
+"projectId": "kss15z"
 }
 
 project Id will be changed per user.
 
 using this command it will execute in headless mode. key also change according to the user. you need to get it from settings tab in the CypressUI.
 
-eg: ./node_modules/.bin/cypress run --record --key 793f1efd-dab8-40e4-9cc0-d4c085ef9f38  
+eg: /node_modules/.bin/cypress run --record --key af206e5a-fad0-41f3-b806-843ac3db4dad 
 
-//comment out the required url in DEV or Staging in Operator_Assistant_Login.js file and run the tests.
-eg:
- cy.logInStag={}
-    cy.logInStag.logInStag =()=>{
-        //cy.visit('https://stg-platform.ai.rakuten-it.com/auth/login');
-        cy.visit('https://admin-ui-dev-3000-esd-aiplatform.dev-zed2-jpe2.dev.jp.local/auth/login');
-    }
-*/
-
-//comment out relavent invitations url dev or stag in Operator Assistant_accounts.js file 
-/*const url="https://admin-ui-dev-3000-esd-aiplatform.dev-zed2-jpe2.dev.jp.local/invitation/"
-const url="https://stg-platform.ai.rakuten-it.com/invitation/"*/
-
-//comment out sso login url dev or stag in plugins->index.js file
-/*await page.goto('https://stg-platform.ai.rakuten-it.com/auth/login');
-//await page.goto('https://admin-ui-dev-3000-esd-aiplatform.dev-zed2-jpe2.dev.jp.local/auth/login');*/
-
-//if the Mail slurp API key get expired by any means you need to create a login in https://app.mailslurp.com/login/ and Refresh a API Key 
-//and put in Operator_assistant_accounts.js => const mailslurp = new MailSlurp({apiKey:"16b85e5cf6b5bfb03a2583cb067e89f010aa023c2bafddc94a54f0cd438c12b2"})
-
+functions aand data re mentioned in support/About_You_Login.js and tests are in integration/examples folder AboutYou.js file. */
